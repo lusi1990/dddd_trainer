8d8df697b72bf33ea7e97933889f589d570240fa
Jack <lusi@seadaka.com>
2022-09-21 10:21:11 +0800

    update

--- captcha_creator/tha_captcha.py
+++ captcha_creator/tha_captcha.py
@@ -1,34 +1,40 @@
 import hashlib
 import os
+import time
 import uuid
 
 import requests
 
 
 def get_captcha(path, num):
-    url = "https://datawarehouse.dbd.go.th/botdetectcaptcha?get=image&c=captchaCode&t=c38b6a2614ae4fb190ac331e3c4177b7&d=1663694135326"
+    url = "https://datawarehouse.dbd.go.th/botdetectcaptcha?get=image&c=captchaCode&t=c38b6a2614ae4fb190ac331e3c4177b7&d={}"
 
     payload = {}
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:105.0) Gecko/20100101 Firefox/105.0',
-        'Accept': 'image/avif,image/webp,*/*',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Connection': 'keep-alive',
-        'Referer': 'https://datawarehouse.dbd.go.th/',
-        'Cookie': 'visid_incap_2466344=C6QfLTfAT7aT4tE/YoNa/EnQ8WIAAAAAQUIPAAAAAABycRb3MoRgad1jWzBCw9k+; JSESSIONID=ZTA3NzEyZWYtNGM5Mi00OGNjLWIwNmQtMTJjYmU1ZGUwZGNh; TS0162c10e=01554f3719cd6f0cb98b2e6c474f768a98aadbc016f9a50ba45ad9532a75c5e83b26ec8f878d456ce0d306c06cbac65b4057f7789ef60a39295e6f882b28e9962b79677b8a; TS45b3629a027=08aef5706eab2000a9ad4e964b8e4a3e3159fc281fca2488fc56919c3cf2c9dd25a10f8792a8e3240813c313c0113000c86888f75451d7c0b72df006136e4c11dfa4c1b770a77547dea3c41c189c50ba7715f1a8b430837b48ac460403619217; incap_ses_1222_2466344=PduxAQlWJ0EroYIsNWv1EJRlKmMAAAAAhH+zLk08Nq3RQfaemE5MLQ==; incap_ses_1556_2466344=Eg3dBRvuRwGQaS5v8gWYFQFnKmMAAAAAg6JjddXSx5AgLNs3WGVgAQ==; TS0162c10e=01554f371997a0bd866e832fc78feaccb16bee77a8822c11e1baf76f98438881e7849bbaa9b77ae560a5861ad844a46a6397be0c880319512940a51209e619646f2796f195; TS45b3629a027=08aef5706eab2000ef8e3ac3e667c32789cc419b7473f0ab9da15787c5897f720ae65c2b8a370f3d086c7ed676113000f5e0ab420eaca80771ae2413fd598af2b7a6e1dd3af5c8828116aa079e0b51b7b98fcdf2076d79f553749b5c5c9e4b02',
-        'Sec-Fetch-Dest': 'image',
-        'Sec-Fetch-Mode': 'no-cors',
-        'Sec-Fetch-Site': 'same-origin',
-        'TE': 'trailers'
+        'authority': 'datawarehouse.dbd.go.th',
+        'accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+        'accept-language': 'en,zh-CN;q=0.9,zh;q=0.8',
+        'cookie': 'JSESSIONID=NTU3NzYxNDAtY2FmMi00NWZhLWExOTItMTc3ZGM4MmJjNjc1; TS0162c10e=01554f37199e80dbc4c66e821d39497af25300e8d64c70fe09216a38c53af5ca0221d1d2d809051365af2e548130b7f1965d4d240d3f4bbc0c895dd712c6de06253654f3df; visid_incap_2466344=qBg47nhmTg6eKtHG+h5KOB1xKmMAAAAAQUIPAAAAAABT0L47xOdVuT38I4iskukk; incap_ses_1046_2466344=U3J5PSX0e3luL/43JiSEDh1xKmMAAAAAgsIBmjBvhkAwXTIyhzlPUA==; TS45b3629a027=08aef5706eab2000b2b212949ca877a11b275032b7eff20365827ffc4f83a3e9bcdde43eb5fcdb020883f36fec113000a85c41cd8ec586d2c237f565dac0455c591597a9eac64590c324ac6c578b4b861f87bba7adce4a8fb46c63483bf117a3',
+        'dnt': '1',
+        'referer': 'https://datawarehouse.dbd.go.th/',
+        'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'image',
+        'sec-fetch-mode': 'no-cors',
+        'sec-fetch-site': 'same-origin',
+        'sec-gpc': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
     }
     session = requests.Session()
     for _ in range(num):
-        response = session.request("GET", url, headers=headers, data=payload)
+        response = session.request("GET", url.format(int(time.time())), headers=headers, data=payload,timeout=5)
         if response.status_code == 200:
             with open(os.path.join(path, f'{uuid.uuid4().hex}.jpeg'), "wb") as f:
                 f.write(response.content)
+        else:
+            print(response.status_code)
 
 
 if __name__ == '__main__':
-    get_captcha('.', 1)
+    get_captcha(r'E:\tha1', 2000)
